394769b2e369eb5590fcff2df006c9e0a357cc8f
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2022-09-10 17:58:53 +0100

    feat: Renamed method. Added console logs for each element (debug purposes). Removed undeclared add method

--- html-css-javascript-getting-started-start/addToCart.js
+++ html-css-javascript-getting-started-start/addToCart.js
@@ -2,23 +2,48 @@
 
 
 var addItemId = 0;
-function addToCart(item){
+function addItemToCart(item) {
     addItemId += 1;
+
+    // print counter, print the selected element
+    console.log({ addItemId, item });
+
     var selectedItem = document.createElement('div');
     selectedItem.classList.add('cartImg');
-    selectedItem.setAttribute('id',addItemId);
+    selectedItem.setAttribute('id', addItemId);
+
     var img = document.createElement('img');
-    img.setAttribute('src',item.children[0].currentsrc);
+    img.setAttribute('src', item.children[0].src);
+    // print product image src
+    console.log("product image src", item.children[0]);
+
     var title = document.createElement('div');
+    title.setAttribute("class", "dog");
     title.innerText = item.children[1].innerText;
+    // print product name
+    console.log("product name", item.children[1]);
+
     var label = document.createElement('div');
-    label.innerText = item.children[2].children[0].innerText;
+    // print price
+    console.log("price", item.children[2]);
+
+    // print price text
+    console.log("price text", item.children[2].innerText);
+    label.innerText = item.children[2].innerText;
+
     var select = document.createElement('span');
-    select.innerText = item.children[2].children[1].value;
+
+    // print the actual value without '£'
+    console.log("value", item.children[2].innerText.substring(1));
     label.append(select);
+
+    var cartItems = document.getElementById('title');
+    // print the element
+    console.log("cartItems", cartItems);
+
     var delBtn = document.createElement('button');
     delBtn.innerText = 'Delete';
-    delBtn.setAttribute('onclick','del('+addItemId+')')
+    delBtn.setAttribute('onclick', 'del(' + addItemId + ')');
     var cartItems = document.getElementById('title');
     selectedItem.append(img);
     selectedItem.append(title);
@@ -30,5 +55,3 @@ function addToCart(item){
 function del(item) {
     document.getElementById(item).remove();
 }
-
-function add(price)
